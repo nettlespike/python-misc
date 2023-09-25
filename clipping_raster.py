3c7aae3f9aba8fa7cfe765cb4e581d997dc4dda1
nettlespike <riverthatflowsfast@gmail.com>
2023-09-25 08:02:30 -0400

    Create clipping_raster.py

---
+++ clipping_raster.py
@@ -0,0 +1,11 @@
+##Tutorial Followed From: https://github.com/daniel-clement/raster-processing/blob/main/Clip_Raster_to_Polygon_Extent_GDAL_Warp_.py
+
+import os
+
+### Build window?
+
+### upload file -> in_raster
+
+### upload file -> polygon shapefile representing geometry you want to clip raster to
+
+###  out_raster -> resulting clipped raster file
